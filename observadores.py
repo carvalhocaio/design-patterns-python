6ad7cdd509d9db4e1848f9426ce4828dc1356f7a
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-02-24 17:15:17 -0400

    Executando diferentes ações e o Observer

---
+++ observadores.py
@@ -0,0 +1,8 @@
+def imprime(nota_fiscal):
+    print(f'Imprimindo Nota Fiscal {nota_fiscal.cnpj}')
+
+def envia_por_email(nota_fiscal):
+    print(f'Enviando Nota Fiscal {nota_fiscal.cnpj} por e-mail')
+
+def salva_no_banco(nota_fiscal):
+    print(f'Salvando Nota Fiscal {nota_fiscal.cnpj} no banco')
